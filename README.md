8400184664cc720cbb540ed7d8241ed85800f01b
nechiporenko <s.nechiporenko@gmail.com>
2016-03-03 20:58:17 +0200

    Header, Menu & Hero added

---
+++ README.md
@@ -0,0 +1,3 @@
+# usaid
+usAID site project.
+Activate in 2 steps: 1)npm install and 2)grunt
---
+++ src/js/main.js
@@ -0,0 +1,228 @@
+// Application Scripts:
+
+// Десктоп меню (выпадайки)
+// Мобильное меню
+// Сообщения об отправке формы
+// Кнопка скролла страницы
+// Модальное окно
+// Если браузер не знает о svg-картинках
+// Если браузер не знает о плейсхолдерах в формах
+
+jQuery(document).ready(function ($) {
+    //Кэшируем
+    var $window = $(window),
+        $html=$('html'),
+        $body = $('body');
+
+    $body.append('<div id="overlay" class="overlay"></div>');
+    var $overlay = $('#overlay');//оверлей
+
+    //
+    // Десктоп меню (выпадайки)
+    //---------------------------------------------------------------------------------------
+    var initDesktopMenu = (function () {
+        $('.js-menu li').on({
+            mouseenter: function () {
+                $(this).find('ul:first').stop(true, true).fadeIn('fast');
+                $(this).find('a:first').addClass('hover');
+            },
+            mouseleave: function () {
+                $(this).find('ul').stop(true, true).fadeOut('slow');
+                $(this).find('a:first').removeClass('hover');
+            }
+        })
+    })();
+
+    //
+    // Мобильное меню
+    //---------------------------------------------------------------------------------------
+    function initMobileMenu() {
+        var $btn = $('.js-mtoggle'),
+            $menu = $('.js-mmenu'),
+            $submenu = $menu.find('.m-submenu'),
+            $s_btn,
+            method = {};
+
+        method.initSubmenu = function () {
+            $menu.find('li').has('ul').addClass('has-menu').append('<button type="button" class="m-menu__btn"><i class="icon-down-open-mini"></i></button>')
+            $s_btn = $menu.find('.m-menu__btn'); //заголовки суб-меню
+        }
+
+        method.hideSubMenu = function () {
+            $s_btn.removeClass('active');
+            $submenu.slideUp();
+        }
+
+        method.hideMenu = function () {
+            $btn.removeClass('active');
+            $menu.removeClass('active');
+            method.hideSubMenu();
+            $html.css('overflow', 'auto');
+            $overlay.unbind('click', method.hideMenu).hide();
+        }
+
+
+        method.showMenu = function () {
+            $btn.addClass('active');
+            $menu.addClass('active');
+            $html.css('overflow', 'hidden');
+            $overlay.show().bind('click', method.hideMenu);
+        }
+
+
+        method.initSubmenu();
+
+        $('.b-header__top').on('click', '.js-mtoggle', function () {//покажем - спрячем
+            if ($(this).hasClass('active')) {
+                method.hideMenu();
+            } else {
+                method.showMenu();
+            }
+        });
+
+        $menu.on('click', '.m-menu__label', method.hideMenu); //закроем по клику по заголовку
+
+        $menu.on('click', '.m-menu__btn', function () {//покажем - спрячем подменю
+            var $el = $(this);
+            
+            if ($el.hasClass('active')) {
+                method.hideSubMenu();
+            } else {
+                method.hideSubMenu();
+                $el.addClass('active').parent('li').find('ul').slideDown();
+            }
+        });
+    }
+    initMobileMenu();
+
+    //
+    // Сообщения об отправке формы
+    //---------------------------------------------------------------------------------------
+    // после аякс-отправки формы ($form), если все ок - $form.find('.g-notice--ok').fadeIn();
+    // если вернуло ошибку - $form.find('.g-notice--bad').fadeIn();
+    var showFormNotice = (function () {
+        var $notice = $('.js-notice');
+        $notice.append('<a class="g-notice__close"><i class="icon-cancel"></i></a>'); //иконка закрытия
+        $notice.on('click', '.g-notice__close', function (e) {//закроем блок по клику на иконку
+            e.preventDefault();
+            $(this).parent('div').fadeOut();
+        });
+    }());
+
+    //
+    // Кнопка скролла страницы
+    //---------------------------------------------------------------------------------------
+    var initPageScroller = (function () {
+        var $scroller = $('<div class="scroll-up-btn"><i class="icon-up-open-big"></i></div>');
+        $body.append($scroller);
+        $window.on('scroll', function () {
+            if ($(this).scrollTop() > 300) {
+                $scroller.show();
+            } else {
+                $scroller.hide();
+            }
+        });
+        $scroller.on('click', function () {
+            $('html, body').animate({ scrollTop: 0 }, 800);
+            return false;
+        });
+    }());
+
+
+    //
+    // Модальное окно
+    //---------------------------------------------------------------------------------------
+    var showModal = (function (link) {
+        var
+        method = {},
+        $modal,
+        $close;
+
+        $close = $('<a class="modal__close" href="#"><i class="icon-cancel"></i></a>'); //иконка закрыть
+
+
+        $close.on('click', function (e) {
+            e.preventDefault();
+            method.close();
+        });
+
+        // центрируем окно
+        method.center = function () {
+            var top, left;
+
+            top = Math.max($window.height() - $modal.outerHeight(), 0) / 2;
+            left = Math.max($window.width() - $modal.outerWidth(), 0) / 2;
+
+            $modal.css({
+                top: top + $window.scrollTop(),
+                left: left + $window.scrollLeft()
+            });
+        };
+
+
+        // открываем
+        method.open = function (link) {
+            $modal = $(link);
+            $modal.append($close);
+            method.center();
+            $window.bind('resize.modal', method.center);
+            $modal.show();
+            $overlay.show().bind('click', method.close);
+        };
+
+        // закрываем
+        method.close = function () {
+            $modal.hide();
+            $overlay.hide().unbind('click', method.close);
+            $window.unbind('resize.modal');
+        };
+
+        return method;
+    }());
+
+    // клик по кнопке с атрибутом data-modal - открываем модальное окно
+    $('[data-modal]').on('click', function (e) {//передаем айди модального окна
+        e.preventDefault();
+        var link = $(this).data('modal');
+        if (link) { showModal.open(link); }
+    });
+
+    //
+    // Если браузер не знает о svg-картинках
+    //---------------------------------------------------------------------------------------
+    if (!Modernizr.svg) {
+        $('img[src*="svg"]').attr('src', function () {
+            return $(this).attr('src').replace('.svg', '.png');
+        });
+    }
+    
+    //
+    // Если браузер не знает о плейсхолдерах в формах
+    //---------------------------------------------------------------------------------------
+    if (!("placeholder" in document.createElement("input"))) {
+        $("input[placeholder], textarea[placeholder]").each(function () {
+            var val = $(this).attr("placeholder");
+            if (this.value == "") {
+                this.value = val;
+            }
+            $(this).focus(function () {
+                if (this.value == val) {
+                    this.value = "";
+                }
+            }).blur(function () {
+                if ($.trim(this.value) == "") {
+                    this.value = val;
+                }
+            })
+        });
+
+        $('form').submit(function () {
+            $(this).find("input[placeholder], textarea[placeholder]").each(function () {
+                if (this.value == $(this).attr("placeholder")) {
+                    this.value = "";
+                }
+            });
+        });
+    }
+    
+});
